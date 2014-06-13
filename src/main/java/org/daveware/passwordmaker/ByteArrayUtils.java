f1369cc51dda80b8c31d8dcdae6aedb1df3300ab
James Stapleton <github@tasermonkeys.com>
2014-06-13 19:28:02 -0400

    Adding the licenses to the new files.  Also added a license file that points to Dave Marotti's source code repository.

--- src/main/java/org/daveware/passwordmaker/ByteArrayUtils.java
+++ src/main/java/org/daveware/passwordmaker/ByteArrayUtils.java
@@ -1,3 +1,21 @@
+/*
+ * PasswordMaker Java Edition - One Password To Rule Them All
+ * Copyright (C) 2014 James Stapleton
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.daveware.passwordmaker;
 
 public final class ByteArrayUtils {
@@ -5,11 +23,9 @@ public final class ByteArrayUtils {
     }
 
     public static String byteArrayToHexString(byte[] bytes) {
-        StringBuilder builder = new StringBuilder();
-        for (byte b : bytes) {
-            builder.append(Character.forDigit(b / 16, 16));
-            builder.append(Character.forDigit(b % 16, 16));
-        }
-        return builder.toString();
+        StringBuilder sb = new StringBuilder(bytes.length*2);
+        for(byte b: bytes)
+            sb.append(Integer.toHexString(b+0x800).substring(1));
+        return sb.toString();
     }
 }
