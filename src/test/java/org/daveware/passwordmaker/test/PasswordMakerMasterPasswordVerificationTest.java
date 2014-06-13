6d931a7cc5de701d2fd58aa474ed4945166207e2
James Stapleton <github@tasermonkeys.com>
2014-06-13 15:03:38 -0400

    This is a test case for the verification code

---
+++ src/test/java/org/daveware/passwordmaker/test/PasswordMakerMasterPasswordVerificationTest.java
@@ -0,0 +1,51 @@
+package org.daveware.passwordmaker.test;
+
+import org.daveware.passwordmaker.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.daveware.passwordmaker.test.TestUtils.saToString;
+import static org.junit.Assert.assertEquals;
+
+public class PasswordMakerMasterPasswordVerificationTest {
+
+    PasswordMaker pwm;
+
+    @Before
+    public void before() {
+        pwm = new PasswordMaker();
+    }
+
+    @Test
+    public void testEmptyMasterPassword() throws Exception {
+        assertEquals("gNV",saToString(pwm.generateVerificationCode(new SecureCharArray(""))));
+    }
+
+    @Test
+    public void testVeryShortMasterPassword() throws Exception {
+        assertEquals("YJO", saToString(pwm.generateVerificationCode(new SecureCharArray("h"))));
+    }
+
+    @Test
+    public void testLongMasterPassword() throws Exception {
+        assertEquals("RHd", saToString(pwm.generateVerificationCode(new SecureCharArray("happybirthday"))));
+    }
+
+    /** This unit test shows that configuring a password maker profile with the same settings that
+     *  the {@link PasswordMaker#generateVerificationCode} uses will generate the same output.
+     *
+     *  This is just a sanity check
+     */
+    @Test
+    public void testPwmWithSameProfileSettings() throws Exception {
+        Account profile = new Account();
+        profile.setCharacterSet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+        profile.setAlgorithm(AlgorithmType.SHA256);
+        profile.setLength(3);
+        profile.setModifier("");
+        profile.setPrefix("");
+        profile.setSuffix("");
+        profile.setLeetType(LeetType.NONE);
+        assertEquals("KPA", saToString(pwm.makePassword(new SecureCharArray("happy"), profile, "")));
+    }
+}
